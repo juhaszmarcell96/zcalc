6b93ddc79106704a8b30f9c11ad4c734ba41f906
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-02-26 21:43:35 +0100

    added superposition test

--- test/network_calculator_test.cpp
+++ test/network_calculator_test.cpp
@@ -100,4 +100,47 @@ TEST(NetworkCalculatorTest, VoltageAndCurrentResponse) {
     ASSERT_EQ(results.at(r_id).currents[0].to_complex(), zcalc::math::Complex(0.0122884, -0.00168902));
     ASSERT_EQ(results.at(r_id).currents[0].get_frequency(), 5000.0);
 }
+
+TEST(NetworkCalculatorTest, SuperpositionTest) {
+    //
+    //                          B(1)       ┌──────┐    C(2)
+    //     ┌────────────────────o──────────┤  R3  ├──────┐
+    //     │                    │          └──────┘      │
+    //   ┌─┴─┐                  │                        └─┐
+    //   │R1 │                  │                        ┌─┘
+    //   │   │                  │                        └─┐
+    //   └─┬─┘                  │                        ┌─┘ L
+    //     │ A(0)               │                        └─┐
+    //  ┌──┼──┐ |             ┌─┴─┐                      ┌─┘
+    //  │  │  │ | U1          │R2 │                      │ D(3)
+    //  │  │  │ |             │   │                      │
+    //  └──┼──┘ v             └─┬─┘                   ┌──┼──┐ |
+    //     │                    │                     │  │  │ | U2
+    //     │                    │  | I                │  │  │ |
+    //     │                    │  V                  └──┼──┘ v
+    //     │       gnd(4)       │                        │
+    //     └────────────────────o────────────────────────┘
+    //
+    zcalc::Network network {};
+    network.add_node ("A");
+    network.add_node ("B");
+    network.add_node ("C");
+    network.add_node ("D");
+    network.add_node ("gnd");
+
+    network.add_voltage_source ("U1", 4.0, 0.0, "A", "gnd"); // 4VDC
+    network.add_voltage_source ("U2", std::sqrt(2.0) * 6.0, 10.0e3 / (2.0 * zcalc::pi), "D", "gnd"); // sqrt(2)* 6V, 10krad/s (1591.5Hz)
+
+    network.add_resistor("R1", 10, "B", "A"); // 10ohm
+    const auto r_id = network.add_resistor("R2", 50, "B", "gnd"); // 50ohm
+    network.add_resistor("R3", 5, "B", "C"); // 5ohm
+    network.add_inductor("L", 1.0e-3, "C", "D"); // 1mH
+
+    const auto results = zcalc::NetworkCalculator::compute(network);
+    ASSERT_EQ(results.at(r_id).currents.size(), 2);
+    ASSERT_EQ(results.at(r_id).currents[0].to_complex(), zcalc::math::Complex(0.025, 0.0));
+    ASSERT_EQ(results.at(r_id).currents[0].get_frequency(), 0.0);
+    ASSERT_NEAR(results.at(r_id).currents[1].to_complex().real(), 0.0678555, 0.0001);
+    ASSERT_NEAR(results.at(r_id).currents[1].to_complex().imag(), -0.0509473, 0.0001);
+    ASSERT_EQ(results.at(r_id).currents[1].get_frequency(), 10.0e3 / (2.0 * zcalc::pi));
 }

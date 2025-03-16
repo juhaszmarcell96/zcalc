43daf54753109de4312e8bbf34620370adbb36eb
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-16 21:06:03 +0100

    junction box

--- client/src/jbox.js
+++ client/src/jbox.js
@@ -2,15 +2,17 @@ class CJunctionBox extends IComponent {
     constructor (x, y) {
         super(x, y, 70 * scale, 70 * scale);
         this.terminals = {
-            T1: new CTerminal(this.x + 15 * scale, this.y + 15 * scale, Colors.l3),
-            T2: new CTerminal(this.x + 35 * scale, this.y + 35 * scale, Colors.l3),
-            T3: new CTerminal(this.x + 55 * scale, this.y + 55 * scale, Colors.l3)
+            T1: new CTerminal(-this.w / 2 + 15 * scale, -this.h / 2 + 15 * scale, Colors.l3),
+            T2: new CTerminal(-this.w / 2 + 35 * scale, -this.h / 2 + 35 * scale, Colors.l3),
+            T3: new CTerminal(-this.w / 2 + 55 * scale, -this.h / 2 + 55 * scale, Colors.l3)
         }
     }
 
     draw (context) {
+        context.translate(this.x + this.w / 2, this.y + this.h / 2);
+        context.rotate(this.angle * Math.PI / 2);
         context.beginPath();
-        context.rect(this.x, this.y, this.w, this.h);
+        context.rect(-this.w / 2, -this.h / 2, this.w, this.h);
         context.fillStyle = Colors.dark_grey;
         context.fill();
         context.lineWidth = this.lineWidth;
@@ -18,6 +20,8 @@ class CJunctionBox extends IComponent {
         context.stroke();
         context.closePath();
         this.draw_terminals(context);
+        context.rotate(-1 * this.angle * Math.PI / 2);
+        context.translate(-(this.x + this.w / 2), -(this.y + this.h / 2));
     }
 
     rotate () {

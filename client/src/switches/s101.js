634c36efced8a7222bb13b6de5ad49ee682f4639
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-16 20:59:20 +0100

    s101

--- client/src/switches/s101.js
+++ client/src/switches/s101.js
@@ -2,34 +2,38 @@ class CSwitch101 extends IComponent {
     constructor (x, y) {
         super(x, y, 90 * scale, 90 * scale);
         this.terminals = {
-            T1: new CTerminal(this.x          - 10 * scale, this.y + this.w / 2, Colors.l3),
-            T2: new CTerminal(this.x + this.w + 10 * scale, this.y + this.w / 2, Colors.l3)
+            T1: new CTerminal(-this.w / 2 +        - 10 * scale, 0, Colors.l3),
+            T2: new CTerminal(-this.w / 2 + this.w + 10 * scale, 0, Colors.l3)
         }
         this.closed = false;
     }
 
     draw (context) {
+        context.translate(this.x + this.w / 2, this.y + this.h / 2);
+        context.rotate(this.angle * Math.PI / 2);
         context.beginPath();
-        context.rect(this.x, this.y, this.w, this.h);
+        context.rect(-this.w / 2, -this.h / 2, this.w, this.h);
         context.fillStyle = Colors.dark_grey; /* grey */
         context.fill();
-        context.moveTo(this.x +  0 * scale, this.y + 50 * scale);
-        context.lineTo(this.x + 35 * scale, this.y + 50 * scale);
+        context.moveTo(-this.w / 2 +  0 * scale, -this.h / 2 + 50 * scale);
+        context.lineTo(-this.w / 2 + 35 * scale, -this.h / 2 + 50 * scale);
         if (this.closed == false) {
-            context.moveTo(this.x + 35 * scale, this.y + 50 * scale);
-            context.lineTo(this.x + 65 * scale, this.y + 30 * scale);
+            context.moveTo(-this.w / 2 + 35 * scale, -this.h / 2 + 50 * scale);
+            context.lineTo(-this.w / 2 + 65 * scale, -this.h / 2 + 30 * scale);
         }
         else {
-            context.moveTo(this.x + 35 * scale, this.y + 50 * scale);
-            context.lineTo(this.x + 65 * scale, this.y + 50 * scale);
+            context.moveTo(-this.w / 2 + 35 * scale, -this.h / 2 + 50 * scale);
+            context.lineTo(-this.w / 2 + 65 * scale, -this.h / 2 + 50 * scale);
         }
-        context.moveTo(this.x + 65 * scale, this.y + 50 * scale);
-        context.lineTo(this.x + 90 * scale, this.y + 50 * scale);
+        context.moveTo(-this.w / 2 + 65 * scale, -this.h / 2 + 50 * scale);
+        context.lineTo(-this.w / 2 + 90 * scale, -this.h / 2 + 50 * scale);
         context.lineWidth = this.lineWidth;
         context.strokeStyle = Colors.black;
         context.stroke();
         context.closePath();
         this.draw_terminals(context);
+        context.rotate(-1 * this.angle * Math.PI / 2);
+        context.translate(-(this.x + this.w / 2), -(this.y + this.h / 2));
     }
 
     rotate () {

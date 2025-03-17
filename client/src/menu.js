958aacd70f71c69b500f3a0e6f92b20c76500ae7
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-17 15:04:57 +0100

    capacitor

--- client/src/menu.js
+++ client/src/menu.js
@@ -6,11 +6,15 @@ class CMenu {
         this.x = x;
         this.y = y;
 
-        this.add_resistor_button = new CButton(100, 100, 85 * scale, 40 * scale, Colors.dark_grey, "resistor");
+        this.add_resistor_button = new CButton(100, 100, 95 * scale, 40 * scale, Colors.dark_grey, "resistor");
+        this.add_capacitor_button = new CButton(100, 150, 95 * scale, 40 * scale, Colors.dark_grey, "capacitor");
 
         this.resistor_img = new Image();
         this.resistor_img.src = 'assets/svg/resistor.svg';
         this.resistor_img.onerror = function() { console.error("failed to load resistor image"); };
+        this.capacitor_img = new Image();
+        this.capacitor_img.src = 'assets/svg/capacitor.svg';
+        this.capacitor_img.onerror = function() { console.error("failed to load capacitor image"); };
 
         canvas.addEventListener('click', (event) => {
             const x = event.clientX - this.x;
@@ -19,11 +23,16 @@ class CMenu {
                 this.scene.components.push(new CResistor(50 * scale, 200 * scale, this.resistor_img));
                 this.scene.redraw();
             }
+            else if (this.add_capacitor_button.is_inside(x, y)) {
+                this.scene.components.push(new CCapacitor(50 * scale, 200 * scale, this.capacitor_img));
+                this.scene.redraw();
+            }
             event.preventDefault();
         });
     }
 
     redraw () {
         this.add_resistor_button.draw(this.context);
+        this.add_capacitor_button.draw(this.context);
     }
 };

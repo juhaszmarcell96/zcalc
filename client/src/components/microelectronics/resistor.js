5512d77afda0b4ac27705e1f3829286bc4186c70
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-17 17:59:06 +0100

    added base class for two-pole components

--- client/src/components/microelectronics/resistor.js
+++ client/src/components/microelectronics/resistor.js
@@ -1,29 +1,5 @@
-class CResistor extends IComponent {
+class CResistor extends CTwoPole {
     constructor (x, y, img) {
-        super(x, y, 90 * scale, 20 * scale);
-        const terminal_0_x = -this.w / 2 - 10 * scale;
-        const terminal_1_x = -this.w / 2 + this.w + 10 * scale;
-        const terminal_0_y = this.h / 2 - 10 * scale;
-        const terminal_1_y = this.h / 2 - 10 * scale;
-        this.terminals = {
-            T1: new CTerminal(terminal_0_x, terminal_0_y, Colors.l3),
-            T2: new CTerminal(terminal_1_x, terminal_1_y, Colors.l3)
-        }
-        this.angle = 0;
-
-        this.img = img
-    }
-
-    draw (context) {
-        context.translate(this.x + this.w / 2, this.y + this.h / 2);
-        context.rotate(this.angle * Math.PI / 2);
-        context.drawImage(this.img, -this.w / 2, -this.h / 2, this.w, this.h + 10 * scale);
-        this.draw_terminals(context);
-        context.rotate(-1 * this.angle * Math.PI / 2);
-        context.translate(-(this.x + this.w / 2), -(this.y + this.h / 2));
-    }
-
-    do_stuff () {
-        // nothing to do
+        super(x, y, img);
     }
 };

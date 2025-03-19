30d12bd3818ca91373332dbc21daf084986d0506
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-19 16:47:33 +0100

    docker container for windows

---
+++ containers/devcontainer/run.ps1
@@ -0,0 +1,8 @@
+docker run --rm -it `
+    --privileged `
+    --name zcalc `
+    --user=root `
+    -p 4444:4444 `
+    -v $PWD/../../:/root/zcalc `
+    --entrypoint /bin/bash `
+    zcalc:active

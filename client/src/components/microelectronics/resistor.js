74def3c8a73d2794ac2b021f7c6835aea82f6aa5
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-04-25 12:59:31 +0200

    resistor serialization

--- client/src/components/microelectronics/resistor.js
+++ client/src/components/microelectronics/resistor.js
@@ -7,5 +7,18 @@ export class CResistor extends CTwoPole {
     constructor (x, y, img) {
         super(x, y, img);
         this.type = ComponentType.resistor;
+        this.resistance = 0;
+    }
+
+    serialize (id) {
+        let json_data = "{";
+        json_data += '"i":' + id.toString()                        + ","; // id
+        json_data += '"t":' + this.type.toString()                 + ","; // type
+        json_data += '"n0":' + this.terminals.T1.nodeId.toString() + ","; // node 0
+        json_data += '"n1":' + this.terminals.T2.nodeId.toString() + ","; // node 1
+        json_data += '"r":' + this.resistance.toString()           + ","; // resistance
+        json_data += '"u":' + this.resistance.toString()           + ","; // unit
+        json_data += '}';
+        return json_data;
     }
 };

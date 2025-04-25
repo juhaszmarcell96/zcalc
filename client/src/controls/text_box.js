74d93ff77a31a7d7138c952699b0f4d73d2d6cfe
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-04-25 14:04:00 +0200

    started adding textbox

---
+++ client/src/controls/text_box.js
@@ -0,0 +1,32 @@
+/* Copyright (C) 2025 Marcell Juhasz. Licensed for non-commercial use. See LICENSE. */
+
+import { Colors, scale } from "../defines";
+
+export class CTextBox {
+    constructor (x, y, w, h, fillStyle, text) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.fillStyle = fillStyle;
+        this.text = text;
+    }
+
+    draw (context) {
+        context.beginPath();
+        context.lineWidth = 2 * scale;
+        context.strokeStyle = Colors.black;
+        context.font = '12pt "Courier New"';
+        context.fillStyle = Colors.black;
+        var lines = this.text.split('\n');
+        for (var i = 0; i<lines.length; i++) {
+            context.fillText(lines[i], this.x + this.w / 2 - (lines[i].length / 2) * 9.5 * scale, this.y + (i + 1) * 25 * scale);
+        }
+        context.stroke();
+        context.closePath();
+    }
+
+    is_inside(pos_x, pos_y) {
+        return pos_x > this.x && pos_x < this.x + this.w && pos_y < this.y + this.h && pos_y > this.y;
+    }
+};

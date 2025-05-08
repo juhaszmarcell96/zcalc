c48f70c98cfdbc4576abb3968bd446470443377e
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-05-08 18:13:22 +0200

    added missing licenses

--- include/zcalc/json/parser.hpp
+++ include/zcalc/json/parser.hpp
@@ -1,4 +1,15 @@
-#pragma once
+/*
+ * Copyright (C) 2025 Marcell Juhasz
+ *
+ * This software is licensed under a non-commercial license.
+ * Non-commercial use (personal, academic, research) is allowed for free.
+ * Commercial use requires a paid license.
+ * Hosting as a service is prohibited without explicit permission.
+ *
+ * See LICENSE for details or contact marcell.juhasz96@gmail.com.
+ */
+
+ #pragma once
 
 #include <iostream>
 #include <fstream>
